6af2e46f347809e7fe57b2dc3416c0875b7349a9
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2021-01-22 23:13:42 -0800

    help details for random question command

--- src/help.js
+++ src/help.js
@@ -5,7 +5,13 @@ const menus = {
         ${chalk.white.bold('quiz [command] <options>')}
         ${chalk.blueBright('version')} ............ show package version
         ${chalk.blueBright('help')} ............... show help menu for a command
+        ${chalk.blueBright('random-questions')} ............... get all random questions
     `,
+
+    'random-questions':`
+        ${chalk.greenBright('quiz random-questions <options>')}
+        --type, -t ..... get questions with type multiple or boolean (true/false)
+    `
 }
 
 export function help(args) {
