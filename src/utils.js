5613d740c338aa70768e4e19a19dd9a1ded53563
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2021-01-23 23:56:34 -0800

    used inquirer for displaying quetions

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,51 @@
 import axios from 'axios'
-import CLI, { Spinner } from 'clui'
+import { Spinner } from 'clui'
+import inquirer from 'inquirer'
+import chalk from 'chalk'
 
-const progress = CLI.Progress
-
-const baseURL = 'https://opentdb.com/api.php?amount=100'
+const baseURL = 'https://opentdb.com/api.php?amount=10'
 
 //Get random questions 
 
 export async function randomQuestions(args){
     let type = args.type || args.t;
+    let amount = args.amount || args.a
     const status = new Spinner("Fetching some questions....")
-
     status.start()
 
     try {
+
         const response = await axios.get(baseURL,{
             params:{
-                type
+                type,
+                amount
             }
         })
+
+        const questions = []
+
+        const questionsProps = {
+            type:'checkbox',
+            validate: function( value ) {
+                if (value.length) {
+                    return true;
+                } else {
+                        return 'Please choose any option';
+                }
+            },
+        }
+
+        response.data.results.map((item) => {
+            let choices = []
+            choices.push(item.correct_answer)
+            choices.push(...item.incorrect_answers)
+            questions.push({...questionsProps, message: `${chalk.green(item.question)}` , name: item.correct_answer, choices })
+        })
+
         status.stop()
-        console.log(response.data)
+
+        const answers = await inquirer.prompt(questions)
+        console.log(answers)
     } catch (error) {
         status.stop()
         console.log("Request got failed with status code: " + error.response.status)
